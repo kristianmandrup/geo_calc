0764fd0e209e53da7016a08ec4fcabf5390e88ac
Kristian Mandrup <kmandrup@gmail.com>
2011-05-12 14:18:31 +0200

    geo_point updated

--- lib/geo_calc/geo_point.rb
+++ lib/geo_calc/geo_point.rb
@@ -21,11 +21,91 @@ class GeoPoint
   # - Numeric lon: longitude in numeric degrees
   # - Numeric [rad=6371]: radius of earth if different value is required from standard 6,371km
 
-  def initializer lat, lon, rad = 6371
+  def initializer *args, rad = 6371
+    case args.size
+    when 1
+      create_from_one *args, rad
+    when 2
+      create_from_two *args, rad
+    else
+      raise "GeoPoint must be initialized with either one or to arguments defining the (latitude, longitude) coordinate on the map"
+    end
+  end
+  
+  protected
+  
+  def create_from_one points, rad = 6371
+    create_from_two *extract_lat_lng(points), rad
+  end
+  
+  def create_from_two lat, lon, rad = 6371  
     rad ||= 6371  # earth's mean radius in km
     # only accept numbers or valid numeric strings
-    @lat    = lat
-    @lon    = lon
+    @lat    = lat.to_lat
+    @lon    = lon.to_lng
     @radius = rad
   end
+  
+  def extract_lat_lng points
+    case points
+    when String
+      points.to_lat_lng 
+    when Array
+      points.to_lat_lng
+    when Hash
+    end
+  end
 end
+
+module NumericLatLngExt
+  def to_lat { self }
+  def to_lng { self }
+end
+
+class Fixnum
+  include NumericLatLngExt
+end
+
+class Float
+  include NumericLatLngExt
+end
+
+class Array
+  def to_lat_lng
+    raise "Array must contain at least two elements to be converted to latitude and longitude" if !size >= 2
+    [first.to_lat, self[1].to_lng]
+  end
+end
+
+class Hash
+  def to_lat_lng
+    [self.to_lat, self.to_lng]
+  end
+  
+  def to_lat
+    [:lat, :latitude].each do |key|
+      return self[key] if self.respond_to? key
+    end
+    raise "Hash must contain either of the keys: [:lat, :latitude] to be converted to a latitude"
+  end
+
+  def to_lng
+    [:lon, :long, :lng, :longitude].each do |key|
+      return self[key] if self.respond_to? key
+    end
+    raise "Hash must contain either of the keys: [:lon, :long, :lng, :longitude] to be converted to a longitude"
+  end
+end  
+
+class String
+  def to_lat_lng  
+    self.split(',').to_lat_lng
+  end
+  
+  def to_lat
+  end
+
+  def to_lng
+  end
+end
+
---
+++ spec/geo_calc/geo_point_spec.rb
@@ -0,0 +1,159 @@
+require 'spec_helper'
+
+# - www.movable-type.co.uk/scripts/latlong.html
+describe GeoPoint do 
+  describe 'initializer' do
+    describe '1 argument' do
+      describe 'single String' do
+        describe '50.1, 5.0 ' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new "50.1, 5.0"
+            p1.should be_a(GeoPoint)
+            p1.lat.should == 50.1
+            p1.lon.should == 5.1
+            p1.unit.should == :degrees
+            p1.radius.should == 6371
+          end
+        end    
+
+        describe '(50.1, 5.0)' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new "(50.1, 5.0)"
+            p1.should be_a(GeoPoint)
+            p1.lat.should == 50.1
+            p1.lon.should == 5.1
+            p1.unit.should == :degrees
+            p1.radius.should == 6371
+          end
+        end 
+
+        describe '58 38 38N, 003 04 12W' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new "58 38 38N, 003 04 12W"
+            p1.should be_a(GeoPoint)
+            p1.lat.should == 50.1
+            p1.lon.should == 5.1
+            p1.unit.should == :degrees
+            p1.radius.should == 6371
+          end
+        end 
+
+        describe '(58 38 38N, 003 04 12W)' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new "(58 38 38N, 003 04 12W)"
+            p1.should be_a(GeoPoint)
+            p1.lat.should be_within(0.5).of(58.38)
+            p1.lon.should be_within(0.5).of(3)
+            p1.unit.should == :degrees
+            p1.radius.should == 6371
+          end
+        end
+      end
+      
+      describe 'single Array' do
+        describe '2 Fixed numbers: 50,5 ' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new [50, 5]
+            p1.should be_a(GeoPoint)
+            p1.lat.should == 50
+            p1.lon.should == 5
+            p1.unit.should == :degrees        
+            p1.radius.should == 6371
+          end
+        end
+        
+        describe '2 Float numbers: 50.1, 5.0 ' do
+          it 'should create a GeoPoint' do
+            p1 = GeoPoint.new [50.1, 5.0]
+            p1.should be_a(GeoPoint)
+            p1.lat.should == 50.1
+            p1.lon.should == 5.1
+            p1.unit.should == :degrees
+            p1.radius.should == 6371
+          end
+        end            
+        
+        describe 'single Hash' do
+          describe 'with: {:lat => 50.1, :lng => 5.0}' do
+            it 'should create a GeoPoint' do
+              p1 = GeoPoint.new {:lat => 50.1, :lng => 5.0}
+              p1.should be_a(GeoPoint)
+              p1.lat.should == 50.1
+              p1.lon.should == 5.1
+              p1.unit.should == :degrees
+              p1.radius.should == 6371
+            end
+          end            
+
+          describe 'with: {:lat => 50.1, :long => 5.0}' do
+            it 'should create a GeoPoint' do
+              p1 = GeoPoint.new {:lat => 50.1, :long => 5.0}
+              p1.should be_a(GeoPoint)
+              p1.lat.should == 50.1
+              p1.lon.should == 5.1
+              p1.unit.should == :degrees
+              p1.radius.should == 6371
+            end
+          end            
+
+          describe 'with: {:latitude => 50.1, :longitude => 5.0}' do
+            it 'should create a GeoPoint' do
+              p1 = GeoPoint.new {:latitude => 50.1, :longitude => 5.0}
+              p1.should be_a(GeoPoint)
+              p1.lat.should == 50.1
+              p1.lon.should == 5.1
+              p1.unit.should == :degrees
+              p1.radius.should == 6371
+            end
+          end
+        end
+      end     
+    end
+    
+    describe 'with 2 arguments' do
+      describe '2 Fixed numbers (Fixnum)' do
+        it 'should create a GeoPoint' do
+          p1 = GeoPoint.new 50, 5
+          p1.should be_a(GeoPoint)
+          p1.lat.should == 50
+          p1.lon.should == 5
+          p1.unit.should == :degrees        
+          p1.radius.should == 6371
+        end
+      end
+
+      describe '2 Float numbers' do
+        it 'should create a GeoPoint' do
+          p1 = GeoPoint.new 50.1, 5.0
+          p1.should be_a(GeoPoint)
+          p1.lat.should == 50.1
+          p1.lon.should == 5.1
+          p1.unit.should == :degrees
+          p1.radius.should == 6371
+        end
+      end    
+      
+      describe '2 Strings: "58 38 38N", "003 04 12W"' do
+        it 'should create a GeoPoint' do
+          p1 = GeoPoint.new "58 38 38N", "003 04 12W"
+          p1.should be_a(GeoPoint)
+          p1.lat.should be_within(0.5).of(58.38)
+          p1.lon.should be_within(0.5).of(3)
+          p1.unit.should == :degrees
+          p1.radius.should == 6371
+        end
+      end
+
+      describe '2 Arrays: [58 38 38N], [003 04 12W]' do
+        it 'should create a GeoPoint' do
+          p1 = GeoPoint.new [58 38 38N], [003 04 12W]
+          p1.should be_a(GeoPoint)
+          p1.lat.should be_within(0.5).of(58.38)
+          p1.lon.should be_within(0.5).of(3)
+          p1.unit.should == :degrees
+          p1.radius.should == 6371
+        end
+      end
+    end
+  end
+end
